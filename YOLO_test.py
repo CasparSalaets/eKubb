7a01c3425145c8c07784601c6201e55f284bfc77
Caspar Salaets <caspar.salaets@gmail.com>
2024-10-15 16:41:26 +0200

    50 epochs gedaan

--- YOLO_test.py
+++ YOLO_test.py
@@ -10,19 +10,18 @@ import os
 import cv2
 import math 
 # start webcam
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3, 640)
 cap.set(4, 480)
 print('camera')
 
 # zelf getrainde model laden
 dir = os.getcwd()
-filePath = os.path.join(dir, 'runs', 'detect', '222f_26v_10e', 'weights', 'best.pt')
+filePath = os.path.join(dir, 'runs', 'detect', '222f_26v_150e', 'weights', 'best.pt')
 model = YOLO(filePath)
 
-# alle klassen waar het model voor getraind 
+# alle klassen waar het model voor getraind 1
 classNames = ['enkel_recht', 'dubbel_recht', 'driedubbel_recht', 'omgevallen', 'koning_recht', 'koning_omgevallen', 'stok']
-
 
 while True:
     success, img = cap.read()
@@ -35,9 +34,6 @@ while True:
             # leest de coordinaten van de box die de ai getekent heeft
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-            # tekent dan de box op het scherm
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 50, 0), 1)
 
             # berekent de confidence dat de ai juist is
             confidence = math.ceil((box.conf[0]*100))/100
@@ -54,7 +50,12 @@ while True:
             color = (0, 0, 255)
             thickness = 1
 
-            cv2.putText(img, (classNames[cls] + " " + str(confidence*100) + "%"), org, font, fontScale, color, thickness)
+            if confidence >= 0.70:
+                # tekent dan de box op het scherm
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 50, 0), 1)
+                print(f'Coordinaten -----> y1: {(y1)}, gem x: {(x1 + x2)/2}')
+
+                cv2.putText(img, (classNames[cls] + " " + str(confidence*100) + "%"), org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
